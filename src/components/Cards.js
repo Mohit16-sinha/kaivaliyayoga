36159af2c0fa7f4e15beb604abbf4423a81273b8
Anand raj <rajanand3307@gmail.com>
2024-11-28 09:36:30 +0530

    sab kuch toh kr diya tha bhai

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-//import images
-
+// Import images
+import card1 from '../assets/img/cards/card-1.png';
+import card2 from '../assets/img/cards/card-2.png';
+import card3 from '../assets/img/cards/card-3.png';
+import card4 from '../assets/img/cards/bg1.png';
 
 const Cards = () => {
-  return <div>Cards</div>;
+  return ( 
+    <section className=''>
+      <div className='container mx-auto'>
+        <div>
+          <div> card1 </div>
+          <div> card2 </div>
+        </div>
+        <div> card3 </div>
+      </div>
+    </section>
+  );
 };
 
 export default Cards;

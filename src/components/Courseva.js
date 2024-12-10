e089c0789ebfffa93063396b8164344b968bde20
Anand raj <rajanand3307@gmail.com>
2024-12-11 01:06:01 +0530

    all is well

--- src/components/Courseva.js
+++ src/components/Courseva.js
@@ -1,14 +1,61 @@
 import React from 'react';
 
 //import data
-import {Courseva} from '../data';
+import {courses} from '../data';
 
 //import icons
-import{} from 'react-icons/bs'
+import{ BsStarFill } from 'react-icons/bs'
 
 
 const Coursevacomponent = () => {
-  return <div>Courses</div>;
+  return <div className='section-sm lg:section-lg'>
+    <div className='container mx-auto'>
+     {/*text */}
+      <div className='text-center mb-16 lg:mb-32'>
+        <h2 className='h2 mb-3 lg:mb-[18px]'>Popular Courses</h2>
+        <p className='max-w-[480px] mx-auto'>
+        Practice anywhere, anytime. Explore a new way 
+        to exercise and learn 
+        more about yourself. We are providing the best.
+        </p>
+      </div>
+      {/*course list */}
+      <div>
+      {courses.map((item,index)=>{
+        //destructure item 
+        const{image,title,desc,link,delay} = item;
+        return <div key={index}>
+          <div>
+            <img src={image} alt=''/>
+          </div>
+          {/* text  */}
+          <div>
+            <h4>{title}</h4>
+            <p>{desc}</p>
+          </div>
+          {/* bottom */}
+          <div>
+            {/* stars */}
+            <div>
+              <BsStarFill/>
+              <BsStarFill/>
+              <BsStarFill/>
+              <BsStarFill/>
+              <BsStarFill/>
+            </div>
+          </div>
+        </div>
+      })}
+      </div>
+
+      {/*btn */}
+
+      <div>
+        <button>Browse all</button>
+      </div>
+
+    </div>
+  </div>;
 };
 
 export default Coursevacomponent;
--- src/data.js
+++ src/data.js
@@ -1,8 +1,36 @@
 // ../data.js
+
+import CourseImage1 from '../src/assets/img/coursepics/course-1.png';
+import CourseImage2 from '../src/assets/img/coursepics/course-2.png';
+import CourseImage3 from '../src/assets/img/coursepics/course-3.png';
+
 export const navigation = [
     { href: "/home", name: "Home" },
     { href: "/about", name: "About" },
     { href: "/services", name: "Services" },
     { href: "/contact", name: "Contact" },
   ];
-  
+
+  export const courses = [
+    {
+      image: CourseImage1,
+      title: 'Resortive Yoga Training & Immersion',
+      desc: 'Here is some tips for new job seekars who want to get a dream job and want to shine in his career.',
+      link: 'Get started',
+      delay: '600',
+    },
+    {
+      image: CourseImage2,
+      title: 'Resortive Yoga Training & Immersion',
+      desc: 'Here is some tips for new job seekars who want to get a dream job and want to shine in his career.',
+      link: 'Get started',
+      delay: '800',
+    },
+    {
+      image: CourseImage3,
+      title: 'Resortive Yoga Training & Immersion',
+      desc: 'Here is some tips for new job seekars who want to get a dream job and want to shine in his career.',
+      link: 'Get started',
+      delay: '900',
+    },
+  ];
